3a72547108fbfdf7e5aff9cae2b3cf3226a63781
satyaswarup140 <dsatyaswarup@gmail.com>
2024-02-23 16:14:07 +0000

    solution

--- src/components/GradientDirectionItem/index.js
+++ src/components/GradientDirectionItem/index.js
@@ -2,23 +2,23 @@
 import {ListItem, DirectionButton} from './styledComponents'
 
 const GradientDirectionItem = props => {
-    const {gradientDirectionDetails, isActive, clickGradientDirectionItem} = props
-    const {displayText, value} = gradientDirectionDetails
-    const onClickGradientDirectionItem = () => {
-        clickGradientDirectionItem(value)
-    }
+  const {gradientDirectionDetails, isActive, clickGradientDirectionItem} = props
+  const {displayText, value} = gradientDirectionDetails
+  const onClickGradientDirectionItem = () => {
+    clickGradientDirectionItem(value)
+  }
 
-    return (
-        <ListItem>
-            <DirectionButton
-              isActive={isActive}
-              type="button"
-              onClick={onClickGradientDirectionItem}
-              >
-                  {displayText}
-              </DirectionButton>
-        </ListItem>
-    )
+  return (
+    <ListItem>
+      <DirectionButton
+        isActive={isActive}
+        type="button"
+        onClick={onClickGradientDirectionItem}
+      >
+        {displayText}
+      </DirectionButton>
+    </ListItem>
+  )
 }
 
-export default GradientDirectionItem 
+export default GradientDirectionItem
--- src/components/GradientDirectionItem/styledComponents.js
+++ src/components/GradientDirectionItem/styledComponents.js
@@ -5,7 +5,7 @@ export const ListItem = styled.li`
   width: 49%;
   list-style-type: none;
   @media screen and (min-width: 768px) {
-      width: 24%;
+    width: 24%;
   }
 `
 export const DirectionButton = styled.button`
